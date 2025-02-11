d20781a60266b34d2a9534d414455d7ac5359bb0
Trung Nguyễn <trung9204@gmail.com>
2025-02-11 18:40:58 +0700

    [Feat]: Select top 5 suitable words

--- test_model.py
+++ test_model.py
@@ -1,16 +1,10 @@
 import torch
-from build_model import LSTMModel
+from build_model import LSTMModel, TransformerModel
 from Vocab_Builder import VocabularyBuilder, DatasetBuilder, ClozeTestDataset
 from torch.utils.data import DataLoader
 from utils import index2word, encode2sentence
 
-vocab_builder = VocabularyBuilder("dataset/dataset_2017.csv")
-vocab_builder.build_vocab()
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-vocab_dict = vocab_builder.get_vocab()
-index_dict = index2word(vocab_dict)
-
-DATASET_PATH = "dataset/dataset_2017.csv"
+DATASET_PATH = "dataset/main_dataset.csv"
 EMBEDDING_DIM = 256
 HIDDEN_DIM = 256
 NUM_LAYERS = 3
@@ -18,28 +12,42 @@ LEARNING_RATE = 0.001
 NUM_EPOCHS = 10
 BATCH_SIZE = 4096
 
+vocab_builder = VocabularyBuilder(DATASET_PATH)
+vocab_builder.build_vocab()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+vocab_dict = vocab_builder.get_vocab()
+index_dict = index2word(vocab_dict)
+
 VOCAB_SIZE = len(vocab_dict.items()) + 3
+# VOCAB_SIZE = 8199
 
-sentences = vocab_builder.get_sentences_list()
+MAX_LEN = vocab_builder.get_longest_sentence_length()
+# MAX_LEN = 27
 
-max_len = vocab_builder.get_longest_sentence_length()
+dataset_builder = DatasetBuilder(vocab_dict, max_len=MAX_LEN)
 
-dataset_builder = DatasetBuilder(vocab_dict, max_len=max_len)
-
-model = LSTMModel(vocab_size=VOCAB_SIZE, embedding_dim=EMBEDDING_DIM, hidden_dim=HIDDEN_DIM, num_layers=NUM_LAYERS, num_classes=VOCAB_SIZE)
-model.load_state_dict(torch.load("trained_model.pth"))
+model = TransformerModel(vocab_size=VOCAB_SIZE, embedding_dim=EMBEDDING_DIM, hidden_dim=HIDDEN_DIM, num_layers=NUM_LAYERS, num_classes=VOCAB_SIZE)
+model.load_state_dict(torch.load("kaggle.pth"))
 model.eval()
 model = model.to(device)
 
 my_sentence = "I go to school with my <mask>"
+my_sentence = "I went to <mask> to buy some milk"
+my_sentence = "He grabbed <mask> keys locked door and headed out for quick run in park"
+my_sentence = "He took book and walked <mask>"
+my_sentence = "After long day at work <mask> decided to relax by taking walk in park and enjoying fresh air"
 
 encode_sentence = dataset_builder.encode_sentence(my_sentence)
 
 encode_sentence = torch.tensor(encode_sentence).to(device).unsqueeze(0)
 
-print(encode_sentence)
 with torch.no_grad():
     outputs = model(encode_sentence)
-    index = torch.argmax(outputs).item()
+    outputs = torch.softmax(outputs, 1)
+    values, indices = torch.topk(outputs, 5)
 
-    print(index_dict[index])    
+    for index in indices[0]:
+        predicted_word = index_dict[index.item()]
+        completed_sentence = my_sentence.replace("<mask>", predicted_word)
+
+        print(completed_sentence)
